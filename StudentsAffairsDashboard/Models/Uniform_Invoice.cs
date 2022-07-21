71617056ffd666b87aa1672a97adbcfff80b29e7
Hossam Nasr <hk2nasr@gmail.com>
2022-07-21 10:50:05 +0200

    added an inhereted InvoicePayment with an item in the list to be edited with its information

---
+++ StudentsAffairsDashboard/Models/Uniform_Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsAffairsDashboard.Models
+{
+    public partial class Uniform_Invoice: invoice_payment
+    {
+        public Uniform_Invoice() : base()
+        {
+            this.date = DateTime.Now;
+            payment_details pd = new payment_details();
+            pd.type = 5;
+            pd.name = "uniform";
+            this.payment_details.Add(pd);
+        }
+    }
+    
+}
